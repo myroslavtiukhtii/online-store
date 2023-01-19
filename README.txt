563bedb8bf3562661b0b03b46355d855e6061956
Myroslav <myroslavtp@gmail.com>
2023-01-19 12:04:42 +0200

    changes

---
+++ README.txt
@@ -0,0 +1,30 @@
+## :pushpin: Online store site
+#### :memo: Description: 
+
+Home page of Product company website.
+___
+
+#### :link: Live Demo: 
+[SITE](https://myroslavtiukhtii.github.io/online-store/dist/)
+
+[Design](The Free Library)
+___
+
+#### :rocket: Technologies Used
+
+* HTML
+* CSS
+* methodology [BEM]
+* Javascript
+* Vue 3
+* SASS
+* Node.js
+* SVG
+* Gulp
+* CSS Animations
+
+___
+
+#### :envelope: Contacts
+* LinkedIn: [@myroslavtiukhtii](https://www.linkedin.com/in/myroslav-t-979a17229)
+* Telegram: [@myroslavtiukhtii](https://t.me/myroslavtiukhtii)
--- dist/js/app.js
+++ dist/js/app.js
@@ -91,43 +91,6 @@
         if (target.hidden) return _slideDown(target, duration); else return _slideUp(target, duration);
     };
     let bodyLockStatus = true;
-    let bodyLockToggle = (delay = 500) => {
-        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
-    };
-    let bodyUnlock = (delay = 500) => {
-        let body = document.querySelector("body");
-        if (bodyLockStatus) {
-            let lock_padding = document.querySelectorAll("[data-lp]");
-            setTimeout((() => {
-                for (let index = 0; index < lock_padding.length; index++) {
-                    const el = lock_padding[index];
-                    el.style.paddingRight = "0px";
-                }
-                body.style.paddingRight = "0px";
-                document.documentElement.classList.remove("lock");
-            }), delay);
-            bodyLockStatus = false;
-            setTimeout((function() {
-                bodyLockStatus = true;
-            }), delay);
-        }
-    };
-    let bodyLock = (delay = 500) => {
-        let body = document.querySelector("body");
-        if (bodyLockStatus) {
-            let lock_padding = document.querySelectorAll("[data-lp]");
-            for (let index = 0; index < lock_padding.length; index++) {
-                const el = lock_padding[index];
-                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            }
-            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
-            document.documentElement.classList.add("lock");
-            bodyLockStatus = false;
-            setTimeout((function() {
-                bodyLockStatus = true;
-            }), delay);
-        }
-    };
     function spollers() {
         const spollersArray = document.querySelectorAll("[data-spollers]");
         if (spollersArray.length > 0) {
@@ -307,10 +270,8 @@
     }
     function menuInit() {
         if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
-            if (bodyLockStatus && e.target.closest(".icon-menu")) {
-                bodyLockToggle();
-                document.documentElement.classList.toggle("menu-open");
-            }
+            if (bodyLockStatus && e.target.closest(".icon-menu")) document.documentElement.classList.toggle("menu-open");
+            if (e.target.closest(".page")) document.documentElement.classList.remove("menu-open");
         }));
     }
     function showMore() {
